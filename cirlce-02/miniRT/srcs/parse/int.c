d84f37d430cecbbbb184823e6a6e192b58b3f2cc
bigpel66 <bigpel66@icloud.com>
2021-04-25 15:19:18 +0900

    parse source files named shortly

--- cirlce-02/miniRT/srcs/parse/int.c
+++ cirlce-02/miniRT/srcs/parse/int.c
@@ -1,60 +1,60 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_val_double.c                                   :+:      :+:    :+:   */
+/*   int.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jseo <jseo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/04/18 23:15:44 by jseo              #+#    #+#             */
-/*   Updated: 2021/04/22 17:08:10 by jseo             ###   ########.fr       */
+/*   Created: 2021/04/18 23:15:48 by jseo              #+#    #+#             */
+/*   Updated: 2021/04/25 15:19:05 by jseo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-t_bool	udouble(char **line, double *v)
+t_bool	uint(char **line, int *v)
 {
-	int	i;
+	t_bool	sign;
 
-	i = 0;
 	if (!line || !(*line))
 		return (FALSE);
-	if (!uint(line, &i) && **line != '.')
-		return (FALSE);
-	*v += (double)i;
-	if (**line == '.')
+	while (is_blank(**line))
+		++(*line);
+	sign = 1;
+	if (**line == '-')
 	{
-		if (!is_digit(*(++(*line))))
-			return (FALSE);
-		i = 0;
-		while (TRUE)
-		{
-			if (is_blank(**line) || !(**line))
-				break ;
-			if (!is_digit(**line))
-				return (FALSE);
-			*v += (1.0 / pow(10.0, ++i)) * (double)(*((*line)++) - '0');
-		}
+		sign = ~(sign) + 1;
+		++(*line);
 	}
-	return (TRUE);
+	if (is_digit(**line))
+		while (is_digit(**line))
+			*v = *v * 10 + (*((*line)++) - '0');
+	else
+		return (FALSE);
+	if (is_blank(**line) || !(**line) || **line == ',')
+	{
+		*v = *v * sign;
+		return (TRUE);
+	}
+	return (FALSE);
 }
 
-t_bool	sdouble(char **line, double *v1, double *v2, double *v3)
+t_bool	sint(char **line, int *v1, int *v2, int *v3)
 {
-	*v1 = 0.0;
-	*v2 = 0.0;
-	*v3 = 0.0;
-	if (!udouble(line, v1) && **line != ',')
+	*v1 = 0;
+	*v2 = 0;
+	*v3 = 0;
+	if (!uint(line, v1) && **line != ',')
 		return (FALSE);
 	if (**line != ',')
 		return (FALSE);
 	++(*line);
-	if (!udouble(line, v2) && **line != ',')
+	if (!uint(line, v2) && **line != ',')
 		return (FALSE);
 	if (**line != ',')
 		return (FALSE);
 	++(*line);
-	if (!udouble(line, v3))
+	if (!uint(line, v3))
 		return (FALSE);
 	return (TRUE);
 }
