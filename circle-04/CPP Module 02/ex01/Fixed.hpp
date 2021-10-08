0f9f7d7b714e2c6e9005e34612878ff549796251
bigpel66 <bigpel66@icloud.com>
2021-10-08 17:36:43 +0900

    01 of module 02

---
+++ circle-04/CPP Module 02/ex01/Fixed.hpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jseo <jseo@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/08 13:29:06 by jseo              #+#    #+#             */
+/*   Updated: 2021/10/08 17:34:07 by jseo             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __FIXED_H__
+# define __FIXED_H__
+
+# include <cmath>
+# include <iostream>
+
+class Fixed
+{
+	private:
+		int						_fixed_point;
+		static const int		_frac_bits = 8;
+
+	public:
+		int						getRawBits(void) const;
+		void					setRawBits(int const raw);
+		float					toFloat(void) const;
+		int						toInt(void) const;
+
+		Fixed&					operator=(const Fixed& f);
+		Fixed(void);
+		Fixed(const int v);
+		Fixed(const float v);
+		Fixed(const Fixed& f);
+		~Fixed(void);
+};
+
+std::ostream&					operator<<(std::ostream& o, const Fixed& f);
+
+#endif
---
+++ circle-04/CPP Module 02/ex01/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jseo <jseo@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/08 16:10:18 by jseo              #+#    #+#             */
+/*   Updated: 2021/10/08 16:10:31 by jseo             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int main(void) {
+    Fixed a;
+    Fixed const b(10);
+    Fixed const c(42.42f);
+    Fixed const d(b);
+
+    a = Fixed(1234.4321f);
+
+    std::cout << "a is " << a << std::endl;
+    std::cout << "b is " << b << std::endl;
+    std::cout << "c is " << c << std::endl;
+    std::cout << "d is " << d << std::endl;
+    std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+    std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+    std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+    std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+    return 0;
+}
