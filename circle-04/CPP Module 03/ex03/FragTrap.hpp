138304a3284b6ba4d68b80d5d25777456c52e8e7
bigpel66 <bigpel66@icloud.com>
2021-10-09 22:11:03 +0900

    plate code of 03 in module 03

--- circle-04/CPP Module 03/ex03/FragTrap.hpp
+++ circle-04/CPP Module 03/ex03/FragTrap.hpp
@@ -6,18 +6,30 @@
 /*   By: jseo <jseo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/09 14:13:32 by jseo              #+#    #+#             */
-/*   Updated: 2021/10/09 14:14:58 by jseo             ###   ########.fr       */
+/*   Updated: 2021/10/09 22:10:29 by jseo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef __FRAG_TRAP_H__
 # define __FRAG_TRAP_H__
 
-class FragTrap
-{
-	private:
+# include "ClapTrap.hpp"
 
+# define F_NAME						"FragTrap"
+# define F_HP						100
+# define F_EP						100
+# define F_AD						30
+
+class FragTrap : public ClapTrap
+{
 	public:
+		void						hiFivesGuys(void) const;
+
+		FragTrap&					operator=(const FragTrap& f);
+		FragTrap(void);
+		FragTrap(const std::string& name);
+		FragTrap(const FragTrap& f);
+		virtual ~FragTrap(void);
 };
 
 #endif
--- circle-04/CPP Module 03/ex03/ScavTrap.hpp
+++ circle-04/CPP Module 03/ex03/ScavTrap.hpp
@@ -6,18 +6,32 @@
 /*   By: jseo <jseo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/09 14:13:32 by jseo              #+#    #+#             */
-/*   Updated: 2021/10/09 14:15:16 by jseo             ###   ########.fr       */
+/*   Updated: 2021/10/09 22:10:48 by jseo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef __SCAV_TRAP_H__
 # define __SCAV_TRAP_H__
 
-class ScavTrap
-{
-	private:
+# include "ClapTrap.hpp"
 
+# define S_NAME						"ScavTrap"
+# define S_HP						100
+# define S_EP						50
+# define S_AD						20
+
+class ScavTrap : public ClapTrap
+{
 	public:
+		void						attack(const std::string& target);
+
+		void						guardGate(void) const;
+
+		ScavTrap&				operator=(const ScavTrap& s);
+		ScavTrap(void);
+		ScavTrap(const std::string& name);
+		ScavTrap(const ScavTrap& s);
+		virtual ~ScavTrap(void);
 };
 
 #endif
