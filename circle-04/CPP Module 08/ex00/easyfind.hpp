8fe8795ef3db73ac78bbec27303d346d1221157d
bigpel66 <bigpel66@icloud.com>
2021-10-15 18:44:24 +0900

    typename changed

--- circle-04/CPP Module 08/ex00/easyfind.hpp
+++ circle-04/CPP Module 08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: jseo <jseo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/14 23:21:19 by jseo              #+#    #+#             */
-/*   Updated: 2021/10/15 15:37:40 by jseo             ###   ########.fr       */
+/*   Updated: 2021/10/15 15:59:16 by jseo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,22 +20,22 @@
 # include <stdexcept>
 # include <vector>
 
-template <typename T>
-typename T::iterator						easyfind(T& container, int value)
+template <typename C>
+typename C::iterator						easyfind(C& container, int value)
 {
-	typename T::iterator iter = std::find(std::begin(container), std::end(container), value);
+	typename C::iterator iter = std::find(std::begin(container), std::end(container), value);
 	if (iter == std::end(container))
-		throw (std::runtime_error(std::string("Value ") + std::to_string(value) + std::string(" is not in Container")));
+		throw (std::runtime_error(std::string("Value ") + std::to_string(value) + std::string(" is not in C")));
 	return (iter);
 }
 
-template <typename T, typename... Arg>
-void										test(T& container, int value, Arg... args)
+template <typename C, typename... Arg>
+void										test(C& container, int value, Arg... args)
 {
-	container = T{args...};
+	container = C{args...};
 	try
 	{
-		typename T::iterator iter = easyfind(container, value);
+		typename C::iterator iter = easyfind(container, value);
 		std::cout << "Value " << value << " found on Index " << std::distance(std::begin(container), iter) << std::endl;
 	}
 	catch (std::exception& e) {
