06bf3dff4c1dd377436159e696112a05a3806e3f
bigpel66 <bigpel66@icloud.com>
2021-12-24 23:32:09 +0900

    test case removed

--- circle-03/minishell/srcs/main.c
+++ circle-03/minishell/srcs/main.c
@@ -6,34 +6,37 @@
 /*   By: jseo <jseo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/22 17:41:44 by jseo              #+#    #+#             */
-/*   Updated: 2021/12/24 23:27:21 by jseo             ###   ########.fr       */
+/*   Updated: 2021/12/24 23:31:47 by jseo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+/* rb tree test case */
+// t_rb *rb = rb_init(compare);
+// rb_insert(rb, jstrdup("a"), jstrdup("1"));
+// rb_insert(rb, jstrdup("b"), jstrdup("2"));
+// rb_insert(rb, jstrdup("x4444"), jstrdup("3"));
+// rb_insert(rb, jstrdup("x22"), jstrdup("4"));
+// rb_insert(rb, jstrdup("x3"), jstrdup("5"));
+// rb_insert(rb, jstrdup("x2"), jstrdup("5"));
+// rb_insert(rb, jstrdup("x"), jstrdup("51"));
+// print_tree(rb);
+// rb_delete(rb, "b");
+// rb_delete(rb, "x2");
+// rb_delete(rb, "x");
+// rb_delete(rb, "a");
+// print_tree(rb);
+// rb_delete(rb, "x4444");
+// rb_delete(rb, "x3");
+// rb_delete(rb, "x22");
+// print_tree(rb);
 
 int main(int argc, char **argv, char **envp)
 {
 	(void)argc;
 	(void)argv;
 	(void)envp;
-	t_rb *rb = rb_init(compare);
-	rb_insert(rb, jstrdup("a"), jstrdup("1"));
-	rb_insert(rb, jstrdup("b"), jstrdup("2"));
-	rb_insert(rb, jstrdup("x4444"), jstrdup("3"));
-	rb_insert(rb, jstrdup("x22"), jstrdup("4"));
-	rb_insert(rb, jstrdup("x3"), jstrdup("5"));
-	rb_insert(rb, jstrdup("x2"), jstrdup("5"));
-	rb_insert(rb, jstrdup("x"), jstrdup("51"));
-	print_tree(rb);
-	rb_delete(rb, "b");
-	rb_delete(rb, "x2");
-	rb_delete(rb, "x");
-	rb_delete(rb, "a");
-	print_tree(rb);
-	rb_delete(rb, "x4444");
-	rb_delete(rb, "x3");
-	rb_delete(rb, "x22");
-    print_tree(rb);
+
 	return (VALID);
 }
