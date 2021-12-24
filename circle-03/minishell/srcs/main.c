8d488e56e81e1df7f41c91b9e5a6ad812b6a74b3
bigpel66 <bigpel66@icloud.com>
2021-12-24 23:28:07 +0900

    Annotation Revised, Delete Leaks Checked, Bug Fix on Free When Deleting a Node

--- circle-03/minishell/srcs/main.c
+++ circle-03/minishell/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jseo <jseo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/22 17:41:44 by jseo              #+#    #+#             */
-/*   Updated: 2021/12/24 22:34:19 by jseo             ###   ########.fr       */
+/*   Updated: 2021/12/24 23:27:21 by jseo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,28 +18,22 @@ int main(int argc, char **argv, char **envp)
 	(void)argv;
 	(void)envp;
 	t_rb *rb = rb_init(compare);
-	rb_insert(rb, "a", "1");
-	rb_insert(rb, "b", "2");
-	rb_insert(rb, "x4444", "3");
-	rb_insert(rb, "x22", "4");
-	rb_insert(rb, "x3", "5");
-	rb_insert(rb, "x2", "5");
-	rb_insert(rb, "x", "51");
-	// rb_insert(rb, "c", "3");
-	// rb_insert(rb, "a", "1");
-	// rb_insert(rb, "abb", "2");
-	// rb_insert(rb, "bbbb", "2");
-	// rb_insert(rb, "bxx", "2");
-	// rb_insert(rb, "avvcx", "3");
-	// rb_insert(rb, "d", "3");
-	// rb_insert(rb, "g", "3");
-	// rb_insert(rb, "z", "3");
-	// rb_insert(rb, "ggggg", "3");
-	// rb_insert(rb, "ggg5", "3");
-	// rb_insert(rb, "x", "3");
+	rb_insert(rb, jstrdup("a"), jstrdup("1"));
+	rb_insert(rb, jstrdup("b"), jstrdup("2"));
+	rb_insert(rb, jstrdup("x4444"), jstrdup("3"));
+	rb_insert(rb, jstrdup("x22"), jstrdup("4"));
+	rb_insert(rb, jstrdup("x3"), jstrdup("5"));
+	rb_insert(rb, jstrdup("x2"), jstrdup("5"));
+	rb_insert(rb, jstrdup("x"), jstrdup("51"));
 	print_tree(rb);
 	rb_delete(rb, "b");
 	rb_delete(rb, "x2");
+	rb_delete(rb, "x");
+	rb_delete(rb, "a");
+	print_tree(rb);
+	rb_delete(rb, "x4444");
+	rb_delete(rb, "x3");
+	rb_delete(rb, "x22");
     print_tree(rb);
 	return (VALID);
 }
--- circle-03/minishell/srcs/rbtree/delete_1.c
+++ circle-03/minishell/srcs/rbtree/delete_1.c
@@ -6,7 +6,7 @@
 /*   By: jseo <jseo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/24 20:38:31 by jseo              #+#    #+#             */
-/*   Updated: 2021/12/24 21:55:37 by jseo             ###   ########.fr       */
+/*   Updated: 2021/12/24 23:27:23 by jseo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 ** return				- void
 ** tree					- RB Tree
 ** key					- Key
-** node					- The Node to be deleted
+** node					- The Node to be Deleted
 ** next					- The Node to Change the Node to be Deleted
 ** child				- The Left / Right Child Node of the Node to be Deleted
 */
@@ -34,6 +34,8 @@ void	rb_delete(t_rb *tree, void *key)
 		return ;
 	if (node->left != NULL && node->right != NULL)
 	{
+		jfree((void **)(&(node->key)));
+		jfree((void **)(&(node->value)));
 		next = get_min(node->right);
 		node->key = next->key;
 		node->value = next->value;
