eceecac931e6c4b82733f1ac19b15982ef6b77c5
bigpel66 <bigpel66@icloud.com>
2022-01-02 01:18:02 +0900

    resolve relative path

--- circle-03/minishell/srcs/exec/command_3.c
+++ circle-03/minishell/srcs/exec/command_3.c
@@ -6,7 +6,7 @@
 /*   By: jseo <jseo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/01 14:05:13 by jseo              #+#    #+#             */
-/*   Updated: 2022/01/01 23:36:26 by jseo             ###   ########.fr       */
+/*   Updated: 2022/01/02 01:17:14 by jseo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,24 @@ char	*find(char *command, t_rb *envmap)
 	return (NULL);
 }
 
-char	*resolve(char *input, t_rb *envmap)
+char	*resolve(char *command, t_rb *envmap)
 {
-	(void)input;
-	(void)envmap;
-	return (NULL);
+	int		ret;
+	char	*sep;
+	char	*path;
+
+	sep = command;
+	while (jstrchr(sep + 1, '/') != NULL)
+		sep = jstrchr(sep + 1, '/');
+	*sep = '\0';
+	ret = chdir(command);
+	*sep = '/';
+	if (ret < 0)
+	{
+		errno = ENOENT;
+		finish(command, true);
+	}
+	path = getcwd(NULL, 0);
+	chdir(get_value(envmap, "PWD"));
+	return (jstrjoin(path, sep));
 }
