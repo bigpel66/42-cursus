437748642420f3b68cd78d9b4355d90a07494ce6
bigpel66 <bigpel66@icloud.com>
2022-01-07 15:30:53 +0900

    indent

--- circle-04/CPP Module 01/ex00/main.cpp
+++ circle-04/CPP Module 01/ex00/main.cpp
@@ -6,15 +6,15 @@ int main(void) {
   Zombie *z[NB_ZOMBIE];
 
   std::cout << "Creating " << NB_ZOMBIE << " Random Heap Zombies"
-        << std::endl << std::endl;
+    << std::endl << std::endl;
   for (int i = 0 ; i < NB_ZOMBIE ; ++i)
     z[i] = Zombie::newZombie(Zombie::randomName());
   std::cout << std::endl << "Creating " << NB_ZOMBIE << " Random Stack Zombies"
-        << std::endl << std::endl;
+    << std::endl << std::endl;
   for (int i = 0 ; i < NB_ZOMBIE ; ++i)
     Zombie::randomChump(Zombie::randomName());
   std::cout << std::endl << "Burying " << NB_ZOMBIE << " Random Heap Zombies"
-        << std::endl << std::endl;
+    << std::endl << std::endl;
   for (int i = 0 ; i < NB_ZOMBIE ; ++i)
     delete z[i];
   return 0;
