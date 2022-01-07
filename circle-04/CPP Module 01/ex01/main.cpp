01987addf5d2fce828d7fdd1457ea7e6ceed7205
bigpel66 <bigpel66@icloud.com>
2022-01-07 15:34:42 +0900

    indent

--- circle-04/CPP Module 01/ex01/main.cpp
+++ circle-04/CPP Module 01/ex01/main.cpp
@@ -4,10 +4,10 @@
 
 int main(void) {
   std::cout << "Creating a Horde of " << NB_ZOMBIE << " Zombies"
-        << std::endl << std::endl;
+    << std::endl << std::endl;
   Zombie *z = Zombie::zombieHorde(NB_ZOMBIE, Zombie::randomName());
   std::cout << std::endl << "Burying a Horde of " << NB_ZOMBIE << " Zombies"
-        << std::endl << std::endl;
+    << std::endl << std::endl;
   delete[] z;
   return 0;
 }
