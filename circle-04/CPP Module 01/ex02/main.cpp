ad570a932e8bdc975cd6da7783e10889143e1345
bigpel66 <bigpel66@icloud.com>
2022-01-07 15:35:54 +0900

    indent

--- circle-04/CPP Module 01/ex02/main.cpp
+++ circle-04/CPP Module 01/ex02/main.cpp
@@ -9,10 +9,10 @@ int main(void) {
   const std::string &stringREF = str;
 
   std::cout << "Address of the original string\t: " << &str << std::endl
-        << "Address of the string in PTR\t: " << stringPTR << std::endl
-        << "Address of the string in REF\t: " << &stringREF << std::endl
-        << std::endl;
+    << "Address of the string in PTR\t: " << stringPTR << std::endl
+    << "Address of the string in REF\t: " << &stringREF << std::endl
+    << std::endl;
   std::cout << "String content by PTR\t\t: " << *stringPTR << std::endl
-        << "String content by REF\t\t: " << stringREF << std::endl;
+    << "String content by REF\t\t: " << stringREF << std::endl;
   return 0;
 }
