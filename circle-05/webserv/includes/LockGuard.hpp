da02a01fc19e8ade949701e045693bca69001cc1
bigpel66 <bigpel66@icloud.com>
2022-05-18 12:32:38 +0900

    lock guard added

---
+++ circle-05/webserv/includes/LockGuard.hpp
@@ -0,0 +1,29 @@
+// Copyright @bigpel66
+
+// Header for the lock guard to automatically unlock based on RAII pattern
+#ifndef CIRCLE_05_WEBSERV_INCLUDES_LOCKGUARD_HPP_
+#define CIRCLE_05_WEBSERV_INCLUDES_LOCKGUARD_HPP_
+
+#include "./Mutex.hpp"  // Lock Guard controls the mutex type
+
+template<typename T>
+class LockGuard {
+ private:
+  T *_mutex;
+
+  LockGuard(void);
+  LockGuard(const LockGuard& g);
+  LockGuard& operator=(const LockGuard& g);
+
+ public:
+  explicit LockGuard(const T& mutex) {
+    _mutex = &mutex;
+    _mutex->lock();
+  }
+
+  ~LockGuard(void) {
+    _mutex->unlock();
+  }
+};
+
+#endif  // CIRCLE_05_WEBSERV_INCLUDES_LOCKGUARD_HPP_
--- circle-05/webserv/includes/Utilizer.hpp
+++ circle-05/webserv/includes/Utilizer.hpp
@@ -17,4 +17,30 @@
 // Socket to Connect TCP for HTTP on C
 // Thread for Concurrency on C
 
+namespace ft {
+  const class nullptr_t {
+   private:
+    // Not allowed to get the address
+    void operator&(void) const;
+
+   public:
+    // Return 0 for any class pointer
+    template<typename T>
+    operator T*(void) const {
+      return 0;
+    }
+
+    // Return 0 for any member pointer
+    template<typename T, typename U>
+    operator T U::*(void) const {
+      return 0;
+    }
+
+    // Safe boolean conversion
+    operator void*(void) const {
+      return 0;
+    }
+  } nullptr_t = {};
+}  // namespace ft
+
 #endif  // CIRCLE_O5_WEBSERV_INCLUDES_UTILIZER_HPP_
