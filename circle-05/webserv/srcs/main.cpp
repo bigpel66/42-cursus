9427d4b02a6d2a90efb950dc2adea4803da31519
bigpel66 <bigpel66@icloud.com>
2022-07-16 14:41:58 +0900

    indent

--- circle-05/webserv/srcs/main.cpp
+++ circle-05/webserv/srcs/main.cpp
@@ -4,8 +4,7 @@
 int main(int argc, char **argv) {
   try {
     Engine e(argc, argv);
-  }
-  catch (std::exception &e) {
+  } catch (std::exception &e) {
     std::cerr << e.what() << std::endl;
   }
   exit(1);
